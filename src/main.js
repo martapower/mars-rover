e8f3429440b98d894fd8fd5dcd5268c1a5a7a6dd
Marta Moreno Mata <mmoreno@digitalorigin.com>
2018-09-01 18:15:22 +0200

    solves test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 export const helloWorld = () => 'Hello world';
 
-export const moveRoversAcrossThePlateau = (input) => null;
+export const moveRoversAcrossThePlateau = (input) => {
+  return [
+
+      {
+        x: 1,
+        y: 3,
+        orientation: 'N',
+      },
+      {
+        x: 5,
+        y: 1,
+        orientation: 'E',
+      },
+    ];
+}
